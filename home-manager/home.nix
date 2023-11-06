16dc11cbd1ce1c6214813b04af7190d6d0881768
Igor Barale <svitaxiom@gmail.com>
2023-11-06 15:26:35 -0800

    remove duplicate git and nvim from home-manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -34,12 +34,11 @@
   };
 
   # Add stuff for your user as you see fit:
-  programs.neovim.enable = true;
+  # programs.neovim.enable = true;
   # home.packages = with pkgs; [ steam ];
 
-  # Enable home-manager and git
+  # Enable home-manager
   programs.home-manager.enable = true;
-  programs.git.enable = true;
 
   # Nicely reload system units when changing configs
   # systemd.user.startServices = "sd-switch";
