46d6363cf2a8ec325cd535155e5782b5e9e12886
Igor Barale <svitaxiom@gmail.com>
2023-11-06 15:11:56 -0800

    init commit

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "NixOS config";
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+
+    # Home manager
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # TODO: Add any other flake you might need
+    # hardware.url = "github:nixos/nixos-hardware";
+
+    # nix-colors.url = "github:misterio77/nix-colors";
+
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , home-manager
+    , ...
+    } @ inputs:
+    let
+      inherit (self) outputs;
+      lib = nixpkgs.lib // home-manager.lib;
+      systems = [ "x86_64-linux" ];
+      forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
+      pkgsFor = lib.genAttrs systems (system: import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      });
+    in
+    {
+      formatter = forEachSystem (pkgs: pkgs.nixpkgs-fmt);
+
+      # NixOS configuration entrypoint
+      # Available through 'nixos-rebuild --flake .#your-hostname'
+      nixosConfigurations = {
+        # FIXME: replace with your hostname
+        nixos = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          # > Our main nixos configuration file <
+          modules = [ ./nixos/configuration.nix ];
+        };
+      };
+    };
+}
---
+++ home-manager/git.nix
@@ -0,0 +1,12 @@
+{ pkgs, lib, config, ...}:
+
+{
+	programs.git = {
+		enable = true;
+		userName = "Igor Barale"
+		userEmail = "svitaxiom@gmail.com"
+		extraConfig = {
+			init.defaultBranch = "main";
+		};
+	};
+}
