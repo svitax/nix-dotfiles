228d0c2ca21c7ad332bebd79e3e99919a8fff394
Igor Barale <svitaxiom@gmail.com>
2023-11-06 16:16:12 -0800

    add fish abbrs for nvim, nixos-rebuild, and home-manager

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -4,6 +4,13 @@
     enable = true;
     interactiveShellInit = ''
     	set fish_greeting # Disable greeting
+    shellAbbrs = {
+    	nixrs = "nixos-rebuild --flake . switch";
+	hms = "home-manager --flake . switch";
+	vim = "nvim";
+	vi = "nvim";
+	v = "nvim";
+    };
 	'';
     plugins = [
     	# Enable a plugin from nixpkgs
