5da511b029109543029657810195d4d38278dc6f
Igor Barale <svitaxiom@gmail.com>
2023-11-06 16:03:06 -0800

    add fish autopair plugin

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -1,5 +1,13 @@
+{ pkgs, ... }:
 {
   programs.fish = {
     enable = true;
+    interactiveShellInit = ''
+    	set fish_greeting # Disable greeting
+	'';
+    plugins = [
+    	# Enable a plugin from nixpkgs
+	{ name = "autopair"; src = pkgs.fishPlugins.autopair.src; }
+    ];
   };
 }
