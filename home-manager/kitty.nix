2510134e82c59fc67a9c6441978d90c9fe2963c5
Igor Barale <svitaxiom@gmail.com>
2023-11-07 21:09:36 -0800

    add kitty scrollback pager with nvim

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -1,10 +1,14 @@
 {pkgs, ...}: {
+  home.file.".config/kitty/pager.sh".source = ./pager.sh;
   programs.kitty = {
     enable = true;
     shellIntegration.enableFishIntegration = true;
     font = {
       name = "JetBrainsMono Nerd Font";
       size = 16;
+    };
+    settings = {
+      scrollback_pager = "~/.config/kitty/pager.sh";
     };
   };
 }
