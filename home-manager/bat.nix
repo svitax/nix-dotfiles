c71fcc8143b3a1d9887a56e771e70cb3736ea6f3
Igor Barale <svitaxiom@gmail.com>
2023-11-09 14:44:37 -0800

    (bat): add gruvbox-dark-hard theme

--- home-manager/bat.nix
+++ home-manager/bat.nix
@@ -1,5 +1,7 @@
 {pkgs, ...}: {
+  home.file.".config/bat/themes/gruvbox-dark-hard.tmTheme".source = ./gruvbox-dark-hard.tmTheme;
   programs.bat = {
     enable = true;
+    config = {theme = "~/.config/bat/themes/gruvbox-dark-hard.tmTheme";};
   };
 }
