ff38efc1250b81d2d05a9db8b17abdc84d4f1dd0
Igor Barale <svitaxiom@gmail.com>
2023-11-09 12:43:49 -0800

    (chore): formatting

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -25,49 +25,49 @@
       fish
       */
       ''
-	# Set default editor because home.sessionVariables and environment.variables doesn't get picked up by fish
-	# due to this https://nix-community.github.io/home-manager/index.html#why_are_the_session_variables_not_set
-	# (home-manager isn't managing my fish shell configuration)
-	set -gx EDITOR "nvim"
-	set -gx VISUAL "nvim"
+        # Set default editor because home.sessionVariables and environment.variables doesn't get picked up by fish
+        # due to this https://nix-community.github.io/home-manager/index.html#why_are_the_session_variables_not_set
+        # (home-manager isn't managing my fish shell configuration)
+        set -gx EDITOR "nvim"
+        set -gx VISUAL "nvim"
 
-        # Use vim bindings and cursors
-        fish_vi_key_bindings
-        set fish_cursor_default block blink
-        set fish_cursor_insert line blink
-        set fish_cursor_replace_one underscore blink
-        set fish_cursor_visual block
+               # Use vim bindings and cursors
+               fish_vi_key_bindings
+               set fish_cursor_default block blink
+               set fish_cursor_insert line blink
+               set fish_cursor_replace_one underscore blink
+               set fish_cursor_visual block
 
-        # `g co`, `npm i`, etc. subcommand expansion with `abbr`.
-        function subcommand_abbr
-          set -l cmd "$argv[1]"
-          set -l short "$argv[2]"
-          set -l long "$argv[3]"
+               # `g co`, `npm i`, etc. subcommand expansion with `abbr`.
+               function subcommand_abbr
+                 set -l cmd "$argv[1]"
+                 set -l short "$argv[2]"
+                 set -l long "$argv[3]"
 
-          # Check that these strings are safe, since we're going to eval. 👺
-          if not string match --regex --quiet '^[a-z]*$' "$short"
-            or not string match --regex --quiet '^[a-z- ]*$' "$long"
-            echo "Scary unsupported alias or expansion $short $long"; exit 1;
-          end
+                 # Check that these strings are safe, since we're going to eval. 👺
+                 if not string match --regex --quiet '^[a-z]*$' "$short"
+                   or not string match --regex --quiet '^[a-z- ]*$' "$long"
+                   echo "Scary unsupported alias or expansion $short $long"; exit 1;
+                 end
 
-          set -l abbr_temp_fn_name (string join "_" "abbr" "$cmd" "$short")
-          # Subcommand arg expanesion via commandline -tokenize + abbr --position anywhere
-          # thx lgarron for inspiration: https://github.com/lgarron/dotfiles/blob/2bc3e0282b/dotfiles/fish/.config/fish/abbr.fish & https://github.com/lgarron/dotfiles/blob/main/dotfiles/fish/.config/fish/dev.fish
-          # https://www.reddit.com/r/fishshell/comments/16s0bsi/leveraging_abbr_for_git_aliases/
-          set -l abbr_temp_fn "function $abbr_temp_fn_name
-            set --local tokens (commandline --tokenize)
-            if test \$tokens[1] = \"$cmd\"
-              echo $long
-            else
-              echo $short
-            end;
-          end;
-          abbr --add $short --position anywhere --function $abbr_temp_fn_name"
-          eval "$abbr_temp_fn"
-        end
+                 set -l abbr_temp_fn_name (string join "_" "abbr" "$cmd" "$short")
+                 # Subcommand arg expanesion via commandline -tokenize + abbr --position anywhere
+                 # thx lgarron for inspiration: https://github.com/lgarron/dotfiles/blob/2bc3e0282b/dotfiles/fish/.config/fish/abbr.fish & https://github.com/lgarron/dotfiles/blob/main/dotfiles/fish/.config/fish/dev.fish
+                 # https://www.reddit.com/r/fishshell/comments/16s0bsi/leveraging_abbr_for_git_aliases/
+                 set -l abbr_temp_fn "function $abbr_temp_fn_name
+                   set --local tokens (commandline --tokenize)
+                   if test \$tokens[1] = \"$cmd\"
+                     echo $long
+                   else
+                     echo $short
+                   end;
+                 end;
+                 abbr --add $short --position anywhere --function $abbr_temp_fn_name"
+                 eval "$abbr_temp_fn"
+               end
 
-        subcommand_abbr git c "commit -am"
-        subcommand_abbr npm i "install"
+               subcommand_abbr git c "commit -am"
+               subcommand_abbr npm i "install"
       '';
     plugins = [
       # Enable a plugin from nixpkgs
