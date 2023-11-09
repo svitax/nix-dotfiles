0816ccb0bffb3c6e2daa1a733ee1e86faf1eafa1
Igor Barale <svitaxiom@gmail.com>
2023-11-09 14:43:51 -0800

    (bat): add bat

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -15,6 +15,7 @@
     inputs.nix-colors.homeManagerModules.default
 
     # You can also split up your configuration and import pieces of it here:
+    ./bat.nix
     ./chromium.nix
     ./fish.nix
     ./fonts.nix
