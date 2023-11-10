696105d7e00ed80d59a974e4e56c82388c836599
Igor Barale <svitaxiom@gmail.com>
2023-11-10 11:05:36 -0800

    (zellij): make zellij a bit more like tmux

--- home-manager/zellij.nix
+++ home-manager/zellij.nix
@@ -3,88 +3,6 @@
   config,
   ...
 }: {
-  home.file.".config/zellij/layouts/default.kdl".text = ''
-    keybinds {
-       shared_except "locked" {
-       	bind "Ctrl y" {
-    	LaunchOrFocusPlugin "file:~/.config/zellij/plugins/zellij_forgot.wasm" {
-    		"lock"                  "ctrl + g"
-    		"unlock"                "ctrl + g"
-    		"new pane"              "ctrl + p + n"
-    		"change focus of pane"  "ctrl + p + arrow key"
-    		"close pane"            "ctrl + p + x"
-    		"rename pane"           "ctrl + p + c"
-    		"toggle fullscreen"     "ctrl + p + f"
-    		"toggle floating pane"  "ctrl + p + w"
-    		"toggle embed pane"     "ctrl + p + e"
-    		"choose right pane"     "ctrl + p + l"
-    		"choose left pane"      "ctrl + p + r"
-    		"choose upper pane"     "ctrl + p + k"
-    		"choose lower pane"     "ctrl + p + j"
-    		"new tab"               "ctrl + t + n"
-    		"close tab"             "ctrl + t + x"
-    		"change focus of tab"   "ctrl + t + arrow key"
-    		"rename tab"            "ctrl + t + r"
-    		"sync tab"              "ctrl + t + s"
-    		"brake pane to new tab" "ctrl + t + b"
-    		"brake pane left"       "ctrl + t + ["
-    		"brake pane right"      "ctrl + t + ]"
-    		"toggle tab"            "ctrl + t + tab"
-    		"increase pane size"    "ctrl + n + +"
-    		"increase pane size"    "ctrl + n + +"
-    		"decrease pane size"    "ctrl + n + -"
-    		"increase pane top"     "ctrl + n + k"
-    		"increase pane right"   "ctrl + n + l"
-    		"increase pane bottom"  "ctrl + n + j"
-    		"increase pane left"    "ctrl + n + h"
-    		"decrease pane top"     "ctrl + n + K"
-    		"decrease pane right"   "ctrl + n + L"
-    		"decrease pane bottom"  "ctrl + n + J"
-    		"decrease pane left"    "ctrl + n + H"
-    		"move pane to top"      "ctrl + h + k"
-    		"move pane to right"    "ctrl + h + l"
-    		"move pane to bottom"   "ctrl + h + j"
-    		"move pane to left"     "ctrl + h + h"
-    		"search"                "ctrl + s + s"
-    		"go into edit mode"     "ctrl + s + e"
-    		"detach session"        "ctrl + o + w"
-    		"open session manager"  "ctrl + o + w"
-    		"quit zellij"           "ctrl + q"
-    		floating true
-    	}
-    }
-       }
-       }
-       layout {
-       	pane
-       	pane size=1 borderless=true {
-       		plugin location="file:${pkgs.zjstatus}/bin/zjstatus.wasm" {
-       		format_left "{mode} #[fg=#89B4FA,bold]{session} {tabs}"
-       		format_right "{command_git_branch}"
-       		format_space ""
-
-       		border_enabled "false"
-       		border_char "-"
-       		border_format "#[fg=#6C7086]{char}"
-       		border_position "top"
-
-       		hide_frame_for_single_pane "true"
-
-       		mode_normal "#[bg=blue,fg=black,bold] {name} "
-       		mode_tmux "#[bg=#ffc387,fg=black,bold] {name} "
-
-       		tab_normal "#[fg=#6C7086] {name} "
-       		tab_active "#[fg=#9399B2,bold,italic] {name} "
-
-       		// {command_NAME} needs zellij 0.39.0 or newer
-       		command_git_branch_command "git rev-parse --abbrev-ref HEAD"
-       		command_git_branch_format "#[fg=blue] {stdout} "
-       		command_git_branch_interval "10"
-       		}
-       	}
-       }
-  '';
-  home.file.".config/zellij/plugins".source = ./config/zellij/plugins;
   programs.zellij = {
     enable = true;
     settings = {
@@ -106,6 +24,128 @@
       };
     };
   };
+  home.file.".config/zellij/plugins".source = ./config/zellij/plugins;
+  home.file.".config/zellij/layouts/default.kdl".text = ''
+    default_mode "normal"
+    keybinds {
+      normal {
+        bind "Alt a" { SwitchToMode "tmux"; }
+        bind "Alt g" { SwitchToMode "locked"; }
+      }
+
+      locked {
+        bind "Alt a" { SwitchToMode "tmux"; }
+        bind "Alt g" { SwitchToMode "normal"; }
+      }
+
+      tmux {
+        bind "Alt a" { SwitchToMode "normal"; }
+        bind "Alt g" { SwitchToMode "locked"; }
+        bind "s" { NewPane "Right"; SwitchToMode "normal"; }
+        bind "v" { NewPane "Down"; SwitchToMode "normal"; }
+        bind "r" { SwitchToMode "RenameTab"; }
+        bind "h" { MoveFocus "Left"; }
+        bind "j" { MoveFocus "Down"; }
+        bind "k" { MoveFocus "Up"; }
+        bind "l" { MoveFocus "Right"; }
+        bind ">" { MovePane; }
+        bind "H" { Resize "Left"; }
+        bind "J" { Resize "Down"; }
+        bind "K" { Resize "Up"; }
+        bind "L" { Resize "Right"; }
+        bind "n" { GoToNextTab; }
+        bind "p" { GoToPreviousTab; }
+        bind "c" { NewTab; SwitchToMode "normal"; }
+        bind "x" { CloseFocus; SwitchToMode "normal"; }
+        bind "e" { EditScrollback; SwitchToMode "normal"; }
+        bind "Esc" "Space" "Enter" { SwitchToMode "normal"; }
+      }
+
+      RenameTab {
+        bind "Alt a" { SwitchToMode "tmux"; }
+        bind "Enter" { SwitchToMode "normal"; }
+        bind "Esc" { UndoRenameTab; SwitchToMode "normal"; }
+      }
+
+      shared_except "locked" {
+        bind "Ctrl y" {
+          LaunchOrFocusPlugin "file:~/.config/zellij/plugins/zellij_forgot.wasm" {
+            "lock"                  "ctrl + g"
+            "unlock"                "ctrl + g"
+            "new pane"              "ctrl + p + n"
+            "change focus of pane"  "ctrl + p + arrow key"
+            "close pane"            "ctrl + p + x"
+            "rename pane"           "ctrl + p + c"
+            "toggle fullscreen"     "ctrl + p + f"
+            "toggle floating pane"  "ctrl + p + w"
+            "toggle embed pane"     "ctrl + p + e"
+            "choose right pane"     "ctrl + p + l"
+            "choose left pane"      "ctrl + p + r"
+            "choose upper pane"     "ctrl + p + k"
+            "choose lower pane"     "ctrl + p + j"
+            "new tab"               "ctrl + t + n"
+            "close tab"             "ctrl + t + x"
+            "change focus of tab"   "ctrl + t + arrow key"
+            "rename tab"            "ctrl + t + r"
+            "sync tab"              "ctrl + t + s"
+            "brake pane to new tab" "ctrl + t + b"
+            "brake pane left"       "ctrl + t + ["
+            "brake pane right"      "ctrl + t + ]"
+            "toggle tab"            "ctrl + t + tab"
+            "increase pane size"    "ctrl + n + +"
+            "increase pane size"    "ctrl + n + +"
+            "decrease pane size"    "ctrl + n + -"
+            "increase pane top"     "ctrl + n + k"
+            "increase pane right"   "ctrl + n + l"
+            "increase pane bottom"  "ctrl + n + j"
+            "increase pane left"    "ctrl + n + h"
+            "decrease pane top"     "ctrl + n + K"
+            "decrease pane right"   "ctrl + n + L"
+            "decrease pane bottom"  "ctrl + n + J"
+            "decrease pane left"    "ctrl + n + H"
+            "move pane to top"      "ctrl + h + k"
+            "move pane to right"    "ctrl + h + l"
+            "move pane to bottom"   "ctrl + h + j"
+            "move pane to left"     "ctrl + h + h"
+            "search"                "ctrl + s + s"
+            "go into edit mode"     "ctrl + s + e"
+            "detach session"        "ctrl + o + w"
+            "open session manager"  "ctrl + o + w"
+            "quit zellij"           "ctrl + q"
+            floating true
+          }
+        }
+      }
+    }
+    layout {
+      pane
+      pane size=1 borderless=true {
+        plugin location="file:${pkgs.zjstatus}/bin/zjstatus.wasm" {
+          format_left "{mode} #[fg=#89B4FA,bold]{session} {tabs}"
+          format_right "{command_git_branch}"
+          format_space ""
+
+          border_enabled "false"
+          border_char "-"
+          border_format "#[fg=#6C7086]{char}"
+          border_position "top"
+
+          hide_frame_for_single_pane "true"
+
+          mode_normal "#[bg=blue,fg=black,bold] {name} "
+          mode_tmux "#[bg=#ffc387,fg=black,bold] {name} "
+
+          tab_normal "#[fg=#6C7086] {name} "
+          tab_active "#[fg=#9399B2,bold,italic] {name} "
+
+          // {command_NAME} needs zellij 0.39.0 or newer
+          command_git_branch_command "git rev-parse --abbrev-ref HEAD"
+          command_git_branch_format "#[fg=blue] {stdout} "
+          command_git_branch_interval "10"
+        }
+      }
+    }
+  '';
   # programs.fish.interactiveShellInit = ''
   #    	if status is-interactive; and type -q zellij
   #   	set -gx ZELLIJ_AUTO_ATTACH true
