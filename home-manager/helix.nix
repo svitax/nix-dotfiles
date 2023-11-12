04f5fcd911c8d85a688694c0d7cb4918922c31cb
Igor Barale <svitaxiom@gmail.com>
2023-11-11 18:18:26 -0800

    (helix): add languages config for python, nix, and toml

--- home-manager/helix.nix
+++ home-manager/helix.nix
@@ -6,6 +6,6 @@
   home.file.".config/helix".source = ./config/helix;
   programs.helix = {
     enable = true;
-    extraPackages = [pkgs.nodePackages.pyright pkgs.nil];
+    extraPackages = [pkgs.nodePackages.pyright pkgs.nil pkgs.taplo];
   };
 }
