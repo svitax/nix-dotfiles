1ea492240b6388a9b17c6763521c9fdde4af800d
Igor Barale <svitaxiom@gmail.com>
2023-11-14 14:33:24 -0800

    (bat): add config through home-manager

--- home-manager/bat.nix
+++ home-manager/bat.nix
@@ -2,6 +2,11 @@
   home.file.".config/bat/themes/gruvbox-dark-hard.tmTheme".source = ./config/bat/themes/gruvbox-dark-hard.tmTheme;
   programs.bat = {
     enable = true;
+    config = {
+      color = "always";
+      style = "numbers";
+      italic-text = "always";
+    };
     # TODO: bat can't find this path for some reason, but yazi does
     # config = {theme = "~/.config/bat/themes/gruvbox-dark-hard.tmTheme";};
   };
