e7a1be0e86b467dbb47d8ded66a2b32bc6407931
Igor Barale <svitaxiom@gmail.com>
2023-11-14 17:57:40 -0800

    (template): add descriptions

--- templates/default.nix
+++ templates/default.nix
@@ -1,6 +1,13 @@
 {
   go.path = ./go;
+  go.description = "";
+
   nodejs.path = ./nodejs;
+  nodejs.description = "";
+
   poetry.path = ./poetry;
+  poetry.description = "";
+
   python.path = ./python;
+  python.description = "";
 }
