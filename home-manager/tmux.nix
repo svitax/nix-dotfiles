5b8c6ee6210bf85b1e1d6ed12c7b7e4f0012ad9d
Igor Barale <svitaxiom@gmail.com>
2023-11-24 14:16:15 -0800

    (tmux): add gitmux

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -45,10 +45,49 @@ in {
   #   source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-dotfiles/home-manager/config/nvim";
   #   recursive = true;
   # };
+  home.packages = with pkgs; [gitmux];
+  home.file.".config/.config/tmux/gitmux.conf".text =
+    /*
+    yml
+    */
+    ''
+      tmux:
+        symbols:
+          branch: "󰘬 "
+          hashprefix: ":"
+          ahead: ↑·
+          behind: ↓·
+          staged: "● "
+          conflict: " "
+          modified: " "
+          untracked: "… "
+          clean: ✔
+          stashed: " "
+          insertions: " "
+          deletions: " "
+        styles:
+          state: "#[fg=#${colors.base08},nobold]"
+          branch: "#[fg=#${colors.base0E},nobold]"
+          staged: "#[fg=#${colors.base0B},nobold]"
+          conflict: "#[fg=#${colors.base08},nobold]"
+          insertions: "#[fg=#${colors.base0B},nobold]"
+          deletions: "#[fg=#${colors.base08},nobold]"
+          modified: "#[fg=#${colors.base0A},nobold]"
+          untracked: "#[fg=#${colors.base03},nobold]"
+          stashed: "#[fg=#${colors.base03},nobold]"
+          clean: "#[fg=#${colors.base0B},nobold]"
+          divergence: "#[fg=#${colors.base0D},nobold]"
+          remote: "#[fg=#${colors.base03},nobold]"
+        layout: [flags, " ", "divergence", " ", branch, .., remote-branch]
+        options:
+          branch_max_len: 0
+          hide_clean: false
+          branch_trim: right
+    '';
   programs.tmux = {
     enable = true;
     shell = "${pkgs.fish}/bin/fish";
-    prefix = "C-a";
+    prefix = "M-a";
     extraConfig =
       /*
       sh
@@ -81,6 +120,36 @@ in {
         # Easier reload of config
         bind o source-file ~/.config/tmux/tmux.conf
 
+        # prefix MAPPINGS
+        bind c new-window -c "#{pane_current_path}" # new window and retain cwd
+        bind C new-session # new session
+        bind d detach-client # detach client
+        bind s split-window -v -c "#{pane_current_path}" # split window horizontally
+        bind v split-window -h -c "#{pane_current_path}" # split window vertically
+        bind x kill-pane # kill pane
+
+        # vim MAPPINGS
+        not_tmux="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?(g?(view|n?vim?x?)(diff)?|fzf)$'"
+        bind-key -n 'M-h' if-shell "$not_tmux" 'send-keys M-h' 'select-pane -R'
+        bind-key -n 'M-j' if-shell "$not_tmux" 'send-keys M-j' 'select-pane -D'
+        bind-key -n 'M-k' if-shell "$not_tmux" 'send-keys M-k' 'select-pane -U'
+        bind-key -n 'M-l' if-shell "$not_tmux" 'send-keys M-l' 'select-pane -L'
+
+        # base MAPPINGS
+        bind -n M-n next-window
+        bind -n M-p previous-window
+
+        # copy-mode MAPPINGS
+        bind -n M-x copy-mode\; send-keys -X start-of-line\; send-keys -X search-backward '#{prompt}' \; send-keys -X jump-backward '#{prompt}'\; send-keys -X next-space \; send-keys -X cursor-right
+        bind-key -T copy-mode-vi 'M-l' select-pane -R
+        bind-key -T copy-mode-vi 'M-j' select-pane -D
+        bind-key -T copy-mode-vi 'M-k' select-pane -U
+        bind-key -T copy-mode-vi 'M-h' select-pane -L
+
+        bind-key -T copy-mode-vi Enter send -X copy-selection-and-cancel
+        bind-key -T copy-mode-vi 'v' send -X begin-selection
+        bind-key -T copy-mode-vi y send-keys -X copy-selection-no-clear 'reattach-to-user-namespace pbcopy'
+
         #  ╭──────────────────────────────────────────────────────────╮
         #  │                    joshmedeski theme                     │
         #  ╰──────────────────────────────────────────────────────────╯
@@ -96,53 +165,52 @@ in {
         color_purple="#${colors.base0E}"
         color_gray="#${colors.base02}"
         color_light_gray="#${colors.base03}"
-        # color_statusline="#171a1a"
+        # color_statusline="#172a1a"
         color_statusline="#${colors.base01}"
 
-        set -g status-style "bg=$color_statusline,fg=$color_fg" # transparent status bar
-        set -g status-position bottom
-        set -g status-justify centre
-
-        set -g status-left-length 90
         set -g status-left "#{tmux_mode_indicator} " # tmux mode
-        set -ga status-left "#[fg=$color_light_gray,bold]#S " # session name
-        # set -ga status-left "#{pomodoro_status}" # pomodoro
-
-        set -g status-right-length 90
-        # set -g status-right "#(gitmux -cfg $HOME/.config/tmux/gitmux.conf '#{pane_current_path}')"
+        set -ga status-left "#[fg=$color_purple,bold]#S " # session name
+        set -g status-left-length 200
         set -g status-right ""
+        # set -g status-right "#[fg=white,nobold]#(gitmux -cfg $HOME/.config/tmux/gitmux.conf '#{pane_current_path}')"
+        set -g status-position bottom
+        set -g status-style "bg=$color_gray,fg=$color_fg"
 
-        setw -g window-status-format "#[fg=$color_light_gray,bg=default]#W"
-        setw -g window-status-current-format "#[fg=$color_fg,bold]#W"
+        setw -g window-status-format "   #[fg=#${colors.base04},bg=default]#W"
+        setw -g window-status-current-format " 󰁔 #[fg=$color_fg,bold]#W"
 
-        setw -g window-status-separator "  "
-        set-window-option -g mode-style bg=$color_purple,fg=$color_bg
-        set -g message-style bg=$color_blue,fg=$color_bg
-
-        set -g pane-active-border-style "fg=$color_fg,bg=default"
+        set -g message-command-style bg=default,fg=yellow
+        set -g message-style bg=default,fg=yellow
+        set -g pane-active-border-style "fg=magenta,bg=default"
         set -g pane-border-style "fg=brightblack,bg=default"
 
         # set -g popup-border-style "fg=#{color_bg},bg=''${color_bg}"
         # set -g popup-border-lines rounded
 
         run-shell ${tmux-mode-indicator}/share/tmux-plugins/tmux-mode-indicator/mode_indicator.tmux
-        # bind -n C-n next-window
-        # bind -n C-p previous-window
       '';
     plugins = with pkgs; [
       tmux-fzf-url
       {
         plugin = tmux-mode-indicator;
         extraConfig = ''
-          # TODO: use nix-colors for this?
-          set -g @mode_indicator_prefix_prompt "▍WAIT"
-          set -g @mode_indicator_prefix_mode_style 'fg=green,bold'
-          set -g @mode_indicator_empty_prompt "▍TMUX"
-          set -g @mode_indicator_empty_mode_style 'fg=orange,bold'
-          set -g @mode_indicator_copy_prompt "▍COPY"
-          set -g @mode_indicator_copy_mode_style 'fg=yellow,bold'
-          set -g @mode_indicator_sync_prompt "▍SYNC"
-          set -g @mode_indicator_sync_mode_style 'fg=red,bold'
+          # set -g @mode_indicator_prefix_prompt "▍WAIT"
+          # set -g @mode_indicator_prefix_mode_style 'fg=green,bold'
+          # set -g @mode_indicator_empty_prompt "▍TMUX"
+          # set -g @mode_indicator_empty_mode_style 'fg=orange,bold'
+          # set -g @mode_indicator_copy_prompt "▍COPY"
+          # set -g @mode_indicator_copy_mode_style 'fg=yellow,bold'
+          # set -g @mode_indicator_sync_prompt "▍SYNC"
+          # set -g @mode_indicator_sync_mode_style 'fg=red,bold'
+
+          set -g @mode_indicator_prefix_prompt "  Wait  "
+          set -g @mode_indicator_prefix_mode_style 'bg=orange,fg=#${colors.base00},bold'
+          set -g @mode_indicator_empty_prompt "  Tmux  "
+          set -g @mode_indicator_empty_mode_style 'bg=green,fg=#${colors.base00},bold'
+          set -g @mode_indicator_copy_prompt "  Copy  "
+          set -g @mode_indicator_copy_mode_style 'bg=yellow,fg=#${colors.base01},bold'
+          set -g @mode_indicator_sync_prompt "  Sync  "
+          set -g @mode_indicator_sync_mode_style 'bg=red,fg=#${colors.base00},bold'
         '';
       }
       {
@@ -159,22 +227,24 @@ in {
           bind -n M-f run-shell "t" # session switcher
         '';
       }
-      # {
-      #   plugin = tmuxPlugins.resurrect;
-      #   extraConfig = ''
-      #     resurrect_dir="$HOME/.tmux/resurrect"
-      #     set -g @resurrect-dir $resurrect_dir
-      #   '';
-      # }
-      # {
-      #   plugin = tmuxPlugins.continuum;
-      #   extraConfig = ''
-      #     set -g @continuum-restore 'on'
-      #     # set -g @continuum-boot 'on'
-      #     # set -g @continuum-save-interval '10'
-      #     # set -g @continuum-systemd-start-cmd 'start-server'
-      #   '';
-      # }
+      {
+        plugin = tmuxPlugins.resurrect;
+        extraConfig = ''
+          set -g @resurrect-capture-pane-contents 'on'
+
+          resurrect_dir="$HOME/.config/tmux/resurrect"
+          set -g @resurrect-dir $resurrect_dir
+        '';
+      }
+      {
+        plugin = tmuxPlugins.continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          # set -g @continuum-boot 'on'
+          # set -g @continuum-save-interval '10'
+          # set -g @continuum-systemd-start-cmd 'start-server'
+        '';
+      }
       # ? joshmedeski/tmux-nerd-font-window-name
       # ? roosta/tmux-fuzzback
       # ? sainnhe/tmux-fzf
