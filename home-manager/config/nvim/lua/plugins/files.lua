90e0c83392a778dd57f6e7863fe7caad2dc9de74
Igor Barale <svitaxiom@gmail.com>
2023-11-24 19:32:07 -0800

    (neovim): add lf.nvim

--- home-manager/config/nvim/lua/plugins/files.lua
+++ home-manager/config/nvim/lua/plugins/files.lua
@@ -1,12 +1,18 @@
 return {
-  -- {
-  --   "echasnovski/mini.nvim",
-  --   config = function()
-  --     require('mini.files').setup({
-  --       windows = {
-  --         preview = true
-  --       }
-  --     })
-  --   end
-  -- }
+  {
+    "lmburns/lf.nvim",
+    dependencies = { "akinsho/toggleterm.nvim" },
+    -- init = function()
+    --   vim.g.lf_netrw = 1
+    -- end,
+    opts = {
+      winblend = 0,
+      escape_quit = false,
+      border = "",
+    },
+    config = function(_, opts)
+      require("lf").setup(opts)
+      vim.keymap.set("n", "<leader>e", "<cmd>Lf<cr>", { desc = "File explorer" })
+    end,
+  },
 }
