8beca5b3c376d9af912ed61f70cb5502a1edf7c6
Igor Barale <svitaxiom@gmail.com>
2023-11-26 18:13:33 -0800

    (kitty): inherit config.colorScheme colors to reduce verbosity

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -1,8 +1,6 @@
-{
-  pkgs,
-  config,
-  ...
-}: {
+{config, ...}: let
+  inherit (config.colorScheme) colors;
+in {
   home.file.".config/kitty/pager.sh".source = ./config/kitty/pager.sh;
   programs.kitty = {
     enable = true;
@@ -19,36 +17,40 @@
       scrollback_pager = "~/.config/kitty/pager.sh";
 
       # Colorscheme
-      foreground = "#${config.colorScheme.colors.base05}";
-      background = "#${config.colorScheme.colors.base00}";
-      selection_foreground = "#${config.colorScheme.colors.base05}";
-      selection_background = "#${config.colorScheme.colors.base00}";
-      url_color = "#${config.colorScheme.colors.base04}";
-      cursor = "#${config.colorScheme.colors.base05}";
-      active_border_color = "#${config.colorScheme.colors.base03}";
-      inactive_border_color = "#${config.colorScheme.colors.base01}";
-      color0 = "#${config.colorScheme.colors.base00}";
-      color1 = "#${config.colorScheme.colors.base08}";
-      color2 = "#${config.colorScheme.colors.base0B}";
-      color3 = "#${config.colorScheme.colors.base0A}";
-      color4 = "#${config.colorScheme.colors.base0D}";
-      color5 = "#${config.colorScheme.colors.base0E}";
-      color6 = "#${config.colorScheme.colors.base0C}";
-      color7 = "#${config.colorScheme.colors.base05}";
-      color8 = "#${config.colorScheme.colors.base03}";
-      color9 = "#${config.colorScheme.colors.base08}";
-      color10 = "#${config.colorScheme.colors.base0B}";
-      color11 = "#${config.colorScheme.colors.base0A}";
-      color12 = "#${config.colorScheme.colors.base0D}";
-      color13 = "#${config.colorScheme.colors.base0E}";
-      color14 = "#${config.colorScheme.colors.base0C}";
-      color15 = "#${config.colorScheme.colors.base07}";
-      color16 = "#${config.colorScheme.colors.base09}";
-      color17 = "#${config.colorScheme.colors.base0F}";
-      color18 = "#${config.colorScheme.colors.base01}";
-      color19 = "#${config.colorScheme.colors.base02}";
-      color20 = "#${config.colorScheme.colors.base04}";
-      color21 = "#${config.colorScheme.colors.base06}";
+      foreground = "#${colors.base05}";
+      background = "#${colors.base00}";
+      selection_foreground = "#${colors.base05}";
+      selection_background = "#${colors.base00}";
+      url_color = "#${colors.base04}";
+      cursor = "#${colors.base05}";
+      active_border_color = "#${colors.base03}";
+      inactive_border_color = "#${colors.base01}";
+      color0 = "#${colors.base00}";
+      color1 = "#${colors.base08}";
+      color2 = "#${colors.base0B}";
+      color3 = "#${colors.base0A}";
+      color4 = "#${colors.base0D}";
+      color5 = "#${colors.base0E}";
+      color6 = "#${colors.base0C}";
+      color7 = "#${colors.base05}";
+      color8 = "#${colors.base03}";
+      color9 = "#${colors.base08}";
+      color10 = "#${colors.base0B}";
+      color11 = "#${colors.base0A}";
+      color12 = "#${colors.base0D}";
+      color13 = "#${colors.base0E}";
+      color14 = "#${colors.base0C}";
+      color15 = "#${colors.base07}";
+      color16 = "#${colors.base09}";
+      color17 = "#${colors.base0F}";
+      color18 = "#${colors.base01}";
+      color19 = "#${colors.base02}";
+      color20 = "#${colors.base04}";
+      color21 = "#${colors.base06}";
     };
+    extraConfig = ''
+      modify_font underline_position +2
+      modify_font underline_thickness 2px
+    '';
   };
 }
