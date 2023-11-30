11ca802528751132e472e60db080bc27be13c921
Igor Barale <svitaxiom@gmail.com>
2023-11-30 10:56:05 -0800

    (fzf): enable fish and tmux integration for fzf

--- home-manager/features/fzf.nix
+++ home-manager/features/fzf.nix
@@ -1,7 +1,8 @@
 {...}: {
   programs.fzf = {
     enable = true;
-    # enableFishIntegration = true;
+    enableFishIntegration = true;
+    tmux.enableShellIntegration = true;
     defaultOptions = ["--multi" "--bind 'alt-a:select-all,alt-d:kill-word,alt-t:toggle-all,ctrl-j:accept,ctrl-k:kill-line,ctrl-n:down,ctrl-p:up,up:previous-history,down:next-history'" "--height 90%" "--reverse" "--tiebreak end"];
   };
 }
