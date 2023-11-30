c31bed068e5ec6914ef932b47dc999b55a492f0f
Igor Barale <svitaxiom@gmail.com>
2023-11-30 10:51:01 -0800

    (templates): add poetry2nix and jupyter templates

--- templates/default.nix
+++ templates/default.nix
@@ -2,11 +2,17 @@
   go.path = ./go;
   go.description = "";
 
+  jupyter.path = ./jupyter;
+  jupyter.description = "";
+
   nodejs.path = ./nodejs;
   nodejs.description = "";
 
   poetry.path = ./poetry;
   poetry.description = "";
+
+  poetry2nix.path = ./poetry2nix;
+  poetry2nix.description = "";
 
   python.path = ./python;
   python.description = "";
