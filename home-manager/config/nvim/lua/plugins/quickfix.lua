e8a79aaeb81ef9cd3519093eb710d46c63baf55e
Igor Barale <svitaxiom@gmail.com>
2023-11-29 22:29:58 -0800

    (neovim): toggle trouble folds with S-Tab

--- home-manager/config/nvim/lua/plugins/quickfix.lua
+++ home-manager/config/nvim/lua/plugins/quickfix.lua
@@ -1,7 +1,12 @@
 return {
   {
     "folke/trouble.nvim",
-    opts = { use_diagnostic_signs = true, mode = "quickfix" },
+    opts = {
+      use_diagnostic_signs = true,
+      mode = "quickfix",
+      indent_lines = false,
+      action_keys = { toggle_fold = { "za", "<S-tab>" } },
+    },
     cmd = { "Trouble", "TroubleToggle" },
     keys = {
       { "<leader>d", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Open diagnostics" },
