5bbd6a908bdc354a89ef7288b74614b889e1fc8d
Igor Barale <svitaxiom@gmail.com>
2023-12-05 20:05:24 -0800

    (neovim): add oil.nvim

--- home-manager/features/config/nvim/lua/util.lua
+++ home-manager/features/config/nvim/lua/util.lua
@@ -190,4 +190,86 @@ function M.get(opts)
   return M.is_win() and ret:gsub("/", "\\") or ret
 end
 
+---Wrapper of nvim_get_hl(), but does not create a cleared highlight group
+---if it doesn't exist
+---NOTE: vim.api.nvim_get_hl() has a side effect, it will create a cleared
+---highlight group if it doesn't exist, see
+---https://github.com/neovim/neovim/issues/24583
+---This affects regions highlighted by non-existing highlight groups in a
+---winbar, which should falls back to the default 'WinBar' or 'WinBarNC'
+---highlight groups but instead falls back to 'Normal' highlight group
+---because of this side effect
+---So we need to check if the highlight group exists before calling
+---vim.api.nvim_get_hl()
+---@param ns_id integer
+---@param opts table{ name: string?, id: integer?, link: boolean? }
+---@return vim.api.keyset.highlight: highlight attributes
+function M.gethl(ns_id, opts)
+  if not opts.name then
+    return vim.api.nvim_get_hl(ns_id, opts)
+  end
+  return vim.fn.hlexists(opts.name) == 1 and vim.api.nvim_get_hl(ns_id, opts) or {}
+end
+
+---@param attr_type 'fg'|'bg'
+---@param fbg? string|integer
+---@param default? integer
+---@return integer|string|nil
+function M.normalize_fg_or_bg(attr_type, fbg, default)
+  if not fbg then
+    return default
+  end
+  local data_type = type(fbg)
+  if data_type == "number" then
+    return fbg
+  end
+  if data_type == "string" then
+    if vim.fn.hlexists(fbg) == 1 then
+      return vim.api.nvim_get_hl(0, {
+        name = fbg,
+        link = false,
+      })[attr_type]
+    end
+    if fbg:match("^#%x%x%x%x%x%x$") then
+      return fbg
+    end
+  end
+  return default
+end
+
+---Normalize highlight attributes
+---1. Replace `attr.fg` and `attr.bg` with their corresponding color codes
+---   if they are set to highlight group names
+---2. If `attr.link` used in combination with other attributes, will first
+---   retrieve the attributes of the linked highlight group, then merge
+---   with other attributes
+---Side effect: change `attr` table
+---@param attr vim.api.keyset.highlight highlight attributes
+---@return table: normalized highlight attributes
+function M.normalize(attr)
+  if attr.link then
+    local num_keys = #vim.tbl_keys(attr)
+    if num_keys <= 1 then
+      return attr
+    end
+    attr.fg = M.normalize_fg_or_bg("fg", attr.fg)
+    attr.bg = M.normalize_fg_or_bg("bg", attr.bg)
+    attr = vim.tbl_extend("force", M.gethl(0, { name = attr.link, link = false }) or {}, attr)
+    attr.link = nil
+    return attr
+  end
+  attr.fg = M.normalize_fg_or_bg("fg", attr.fg)
+  attr.bg = M.normalize_fg_or_bg("bg", attr.bg)
+  return attr
+end
+
+---Wrapper of nvim_set_hl(), normalize highlight attributes before setting
+---@param ns_id integer namespace id
+---@param name string
+---@param attr vim.api.keyset.highlight highlight attributes
+---@return nil
+function M.set(ns_id, name, attr)
+  return vim.api.nvim_set_hl(ns_id, name, M.normalize(attr))
+end
+
 return M
