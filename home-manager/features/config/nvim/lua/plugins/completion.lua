d34de564a79a9ecaeb447c81ce711321790f7728
Igor Barale <svitaxiom@gmail.com>
2023-12-05 20:15:32 -0800

    (neovim): massive restructuring of nvim-cmp

--- home-manager/features/config/nvim/lua/plugins/completion.lua
+++ home-manager/features/config/nvim/lua/plugins/completion.lua
@@ -19,22 +19,25 @@ return {
       end,
     },
   },
+  { "hrsh7th/cmp-cmdline", event = "CmdlineEnter", dependencies = "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp", event = "InsertEnter", dependencies = { "hrsh7th/nvim-cmp", "neovim/nvim-lspconfig" } },
+  { "hrsh7th/cmp-path", event = { "CmdlineEnter", "InsertEnter" }, dependencies = { "hrsh7th/nvim-cmp" } },
+  { "hrsh7th/cmp-buffer", event = { "CmdlineEnter", "InsertEnter" }, dependencies = "hrsh7th/nvim-cmp" },
+  { "rcarriga/cmp-dap", lazy = true, dependencies = { "mfussenegger/nvim-dap", "hrsh7th/nvim-cmp" } },
+  { "saadparwaiz1/cmp_luasnip", event = "InsertEnter", dependencies = { "hrsh7th/nvim-cmp", "L3MON4D3/LuaSnip" } },
+  { "lukas-reineke/cmp-under-comparator", event = "InsertEnter", dependencies = "hrsh7th/nvim-cmp" },
+  {
+    "petertriho/cmp-git",
+    ft = { "NeogitCommitMessage", "gitcommit", "octo" },
+    dependencies = "hrsh7th/nvim-cmp",
+    opts = { filetypes = { "NeogitCommitMessage", "gitcommit", "octo" } },
+  },
   -- Autocompletion
   {
     "hrsh7th/nvim-cmp",
     lazy = true,
     version = false,
     event = { "InsertEnter", "CmdlineEnter" },
-    dependencies = {
-      "FelipeLema/cmp-async-path",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/cmp-nvim-lsp",
-      "saadparwaiz1/cmp_luasnip",
-      "lukas-reineke/cmp-under-comparator", -- TODO: still using default sorting
-      "rcarriga/cmp-dap",
-      { "petertriho/cmp-git", opts = { filetypes = { "NeogitCommitMessage", "gitcommit", "octo" } } },
-    },
     -- Configure nvim-cmp
     -- See `:help cmp`
     opts = function(_, opts)
@@ -42,9 +45,8 @@ return {
 
       local cmp = require("cmp")
       local luasnip = require("luasnip")
-      local defaults = require("cmp.config.default")()
 
-      local t = function(str)
+      local function t(str)
         return vim.api.nvim_replace_termcodes(str, true, true, true)
       end
 
@@ -60,14 +62,24 @@ return {
       --
       --   return contents
       -- end
+      ---Filter out unwanted entries
+      ---@param entry cmp.Entry
+      ---@param _ cmp.Context ignored
+      ---@return boolean
+      local function entry_filter(entry, _)
+        return not vim.tbl_contains({
+          "No matches found",
+          "Searching...",
+          "Workspace loading",
+        }, entry.completion_item.label)
+      end
 
       local cmp_source_names = {
-        async_path = "path",
+        path = "path",
         buffer = "buffer",
         cmdline = "cmd",
         luasnip = "snippet",
         nvim_lsp = "lsp",
-        obsidian = "obsidian",
         dap = "dap",
         git = "git",
         -- obsidian.nvim automatically registers its sources. define source names else my config errors
@@ -79,6 +91,7 @@ return {
       }
 
       return {
+        -- cmp floating window config
         window = {
           completion = {
             winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
@@ -87,6 +100,24 @@ return {
           },
           documentation = {
             winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+            max_width = 80,
+            max_height = 20,
+          },
+        },
+        performance = { max_view_entries = 64 },
+        experimental = { ghost_text = { hl_group = "CmpGhostText" } },
+        sorting = {
+          priority_weight = 2,
+          comparators = {
+            ---@type table[]|function[]
+            comparators = {
+              require("cmp-under-comparator").under,
+              cmp.config.compare.kind,
+              cmp.config.compare.locality,
+              cmp.config.compare.recently_used,
+              cmp.config.compare.exact,
+              cmp.config.compare.score,
+            },
           },
         },
         -- enable cmp in dap buffers
@@ -151,16 +182,20 @@ return {
           end, { "i", "s", "c" }),
         }),
         sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "async_path", option = { trailing_slash = true } },
+          { name = "luasnip", max_item_count = 3 },
+          {
+            name = "nvim_lsp",
+            max_item_count = 20,
+            entry_filter = entry_filter, -- Suppress LSP completion when workspace is not ready yet
+          },
           { name = "buffer", keyword_length = 4, max_item_count = 5, group_index = 2 },
+          { name = "path", entry_filter = entry_filter },
           { name = "git" },
         }),
         cmdline = {
           sources = {
-            { name = "cmdline", max_item_count = 30 },
-            { name = "async_path", max_item_count = 20 },
+            { name = "path", entry_filter = entry_filter },
+            { name = "cmdline", option = { ignore_cmds = {} }, group_index = 2, max_item_count = 30 },
           },
           formatting = { max_width = 30 },
           -- diable cmdline completion for certain commands, such as IncRename
@@ -176,12 +211,13 @@ return {
             return not disabled[cmd] or cmp.close()
           end,
         },
-        cmdline_search = {
-          -- view = {
-          --   entries = { name = "wildmenu", separator = "|" }
-          -- },
+        cmdline_search = { sources = { { name = "buffer" } } },
+        -- Complete vim.ui.input()
+        ui_input = {
           sources = {
-            { name = "buffer" },
+            { name = "path", group_index = 1, entry_filter = entry_filter },
+            { name = "cmdline", group_index = 2, option = { ignore_cmds = {} } },
+            { name = "buffer", group_index = 3 },
           },
         },
         formatting = {
@@ -211,12 +247,6 @@ return {
             return item
           end,
         },
-        experimental = {
-          ghost_text = {
-            hl_group = "CmpGhostText",
-          },
-        },
-        sorting = defaults.sorting,
       }
     end,
     config = function(_, opts)
@@ -226,18 +256,21 @@ return {
 
       require("cmp").setup(opts)
       require("cmp").setup.cmdline(":", opts.cmdline)
-      require("cmp").setup.cmdline({ "/", "?", "@" }, opts.cmdline_search)
-      -- stylua: ignore
-      require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, { sources = { { name = "dap" }, }, })
+      require("cmp").setup.cmdline({ "/", "?" }, opts.cmdline_search)
+      require("cmp").setup.cmdline({ "@" }, opts.ui_input)
+      require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, { sources = { { name = "dap" } } })
     end,
   },
   -- {
   --   "ray-x/lsp_signature.nvim",
   --   opts = {
+  --     hint_enable = true,
+  --     floating_window = false,
+  --     hint_inline = function()
+  --       return "eol"
+  --     end,
+  --     hint_scheme = "Comment",
   --     hi_parameter = "PmenuSel",
-  --     hint_enable = false,
-  --     -- hint_inline = true, -- need neovim >= 0.10
-  --     handler_opts = { border = "none" },
   --   },
   -- },
   -- Autopairing
