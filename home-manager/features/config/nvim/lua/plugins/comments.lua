4adbbbfd6ea462f0b10e5bae99384d45d86c9f45
Igor Barale <svitaxiom@gmail.com>
2023-12-05 19:51:51 -0800

    (neovim): lazy load comment.nvim

--- home-manager/features/config/nvim/lua/plugins/comments.lua
+++ home-manager/features/config/nvim/lua/plugins/comments.lua
@@ -1,4 +1,8 @@
 return {
   -- "gc" to comment visual regions/lines
-  { "numToStr/Comment.nvim", opts = { toggler = { line = "<C-c>" }, opleader = { line = "<C-c>" } } },
+  {
+    "numToStr/Comment.nvim",
+    opts = { toggler = { line = "<C-c>" }, opleader = { line = "<C-c>" } },
+    keys = { "<C-c>" },
+  },
 }
