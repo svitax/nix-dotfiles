bd3fe4948656e7b396f781cdf5deb48f0c432c7e
Igor Barale <svitaxiom@gmail.com>
2023-12-18 20:34:28 -0800

    (templates): format rust flake template

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -6,30 +6,29 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = {
-    nixpkgs,
-    rust-overlay,
-    ...
-  } @ inputs: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = [rust-overlay.overlays.default];
+  outputs = { nixpkgs, rust-overlay, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ rust-overlay.overlays.default ];
+      };
+      toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
+    in {
+      formatter = pkgs.alejandra;
+      devShells.${system}.default = pkgs.mkShell {
+        name = "nix-rust-template";
+        packages = [
+          toolchain
+          # We want the unwrapped version, "rust-analyzer" (wrapped) comes with nixpkgs' toolchain
+          pkgs.rust-analyzer-unwrapped
+          pkgs.vscode-extensions.vadimcn.vscode-lldb.adapter # debugging adapter
+          pkgs.taplo # toml language server
+        ];
+        env = {
+          RUST_BACKTRACE = "full";
+          RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+        };
+      };
     };
-    toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
-  in {
-    formatter = pkgs.alejandra;
-    devShells.${system}.default = pkgs.mkShell {
-      name = "nix-rust-template";
-      packages = [
-        toolchain
-        # We want the unwrapped version, "rust-analyzer" (wrapped) comes with nixpkgs' toolchain
-        pkgs.rust-analyzer-unwrapped
-        pkgs.vscode-extensions.vadimcn.vscode-lldb.adapter # debugging adapter
-        pkgs.taplo # toml language server
-      ];
-      env = {RUST_BACKTRACE = "full";};
-      RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
-    };
-  };
 }
