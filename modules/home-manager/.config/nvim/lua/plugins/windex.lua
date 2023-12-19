ed878a2098ca1c932a3fb8169b20236780dd27a9
Igor Barale <svitaxiom@gmail.com>
2023-12-18 17:39:13 -0800

    (neovim): use A+hjkl to switch windows

--- modules/home-manager/.config/nvim/lua/plugins/windex.lua
+++ modules/home-manager/.config/nvim/lua/plugins/windex.lua
@@ -3,10 +3,10 @@ return {
         "declancm/windex.nvim",
         opts = { default_keymaps = false },
         keys = {
-            { "<C-h>", "<cmd>lua require('windex').switch_window('left')<cr>", desc = "Switch window left" },
-            { "<C-j>", "<cmd>lua require('windex').switch_window('down')<cr>", desc = "Switch window down" },
-            { "<C-k>", "<cmd>lua require('windex').switch_window('up')<cr>", desc = "Switch window up" },
-            { "<C-l>", "<cmd>lua require('windex').switch_window('right')<cr>", desc = "Switch window right" },
+            { "<A-h>", "<cmd>lua require('windex').switch_window('left')<cr>", desc = "Switch window left" },
+            { "<A-j>", "<cmd>lua require('windex').switch_window('down')<cr>", desc = "Switch window down" },
+            { "<A-k>", "<cmd>lua require('windex').switch_window('up')<cr>", desc = "Switch window up" },
+            { "<A-l>", "<cmd>lua require('windex').switch_window('right')<cr>", desc = "Switch window right" },
         },
     },
 }
