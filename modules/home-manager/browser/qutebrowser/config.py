55fc17d976617ce8b65e495a8431fe447230e80b
svitax <svitaxiom@gmail.com>
2025-06-14 20:52:40 -0400

    (qutebrowser): prepare some extra config

--- modules/home-manager/browser/qutebrowser/config.py
+++ modules/home-manager/browser/qutebrowser/config.py
@@ -326,3 +326,24 @@ c.content.blocking.adblock.lists = [
 ]
 
 c.content.autoplay = False
+
+# TODO extra config
+# config.set('content.cookies.accept', 'no-3rdparty', 'chrome-devtools://*')
+# config.set('content.cookies.accept', 'no-3rdparty', 'devtools://*')
+
+# config.set('content.images', True, 'chrome-devtools://*')
+# config.set('content.images', True, 'devtools://*')
+
+# config.set('content.javascript.enabled', True, 'chrome-devtools://*')
+# config.set('content.javascript.enabled', True, 'devtools://*')
+# config.set('content.javascript.enabled', True, 'chrome://*/*')
+# config.set('content.javascript.enabled', True, 'qute://*/*')
+
+# config.set('content.notifications.enabled', False, 'https://www.reddit.com')
+# config.set('content.notifications.enabled', False, 'https://www.youtube.com')
+
+# config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}', 'https://web.whatsapp.com/')
+# config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/119.0', 'https://accounts.google.com/*')
+# config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info} rv:110.0) Gecko/20100101 Firefox/119.0', 'https://*.slack.com/*')
+# config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/119.0', 'https://docs.google.com/*')
+# config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/119.0', 'https://drive.google.com/*')
