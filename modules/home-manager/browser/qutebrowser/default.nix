41c2ce17315087fedc9d5df01ea84760245e8747
svitax <svitaxiom@gmail.com>
2025-06-14 20:48:17 -0400

    (qutebrowser): add greasemonkey scripts

--- modules/home-manager/browser/qutebrowser/default.nix
+++ modules/home-manager/browser/qutebrowser/default.nix
@@ -21,5 +21,11 @@ in
 
     home.file.".config/qutebrowser/config.py".source = ./config.py;
     home.file.".config/qutebrowser/all-sites.css".source = ./all-sites.css;
+    # Make sure to do `:greasemonkey-reload' every time you change or add a
+    # script.
+    home.file.".config/qutebrowser/greasemonkey" = {
+      source = ./greasemonkey;
+      recursive = true;
+    };
   };
 }
