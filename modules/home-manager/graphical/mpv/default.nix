52d2e1ad6fc8770162061a2dc708f7b906e8d5f4
svitax <svitaxiom@gmail.com>
2025-06-19 18:32:43 -0400

    (mpv): set hwdec to auto-safe

--- modules/home-manager/graphical/mpv/default.nix
+++ modules/home-manager/graphical/mpv/default.nix
@@ -44,6 +44,7 @@ in
         save-watch-history = true;
         write-filename-in-watch-later-config = true;
         watch-later-directory = "${config.xdg.stateHome}/mpv/watch_later";
+        hwdec = "auto-safe";
         ytdl-format = "(bestvideo[vcodec^=av01][height<=?2160]/bestvideo[height<=?2160])+bestaudio/best";
       };
       scriptOpts = {
