93a76fe5bed024843c4fb1cb14ad4632c2da91d6
svitax <svitaxiom@gmail.com>
2025-10-08 11:58:56 -0400

    (storage): add maestral

--- hosts/erasmus/home.nix
+++ hosts/erasmus/home.nix
@@ -58,6 +58,21 @@
 
   # window-manager.sway.enable = true;
 
+  # TODO: extract maestral module
+  systemd.user.services.maestral = {
+    Unit = {
+      Description = "Maestral daemon";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+    Service = {
+      ExecStart = "${pkgs.maestral}/bin/maestral start --foreground";
+      ExecStop = "${pkgs.maestral}/bin/maestral stop";
+      Restart = "on-failure";
+    };
+  };
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "evermind";
@@ -80,6 +95,8 @@
     # pkgs.hello
     # TODO extract home.packages into modules
     runelite
+    maestral
+    maestral-gui
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
