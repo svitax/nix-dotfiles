78fe61ec314e9dc7c4e737834dd9b6d80d9b58aa
svitax <svitaxiom@gmail.com>
2025-10-20 21:13:18 -0400

    (xfce): add extra xfce and thunar plugins

--- modules/nixos/desktop-manager/xfce/default.nix
+++ modules/nixos/desktop-manager/xfce/default.nix
@@ -21,11 +21,13 @@ in
     # Enable the XFCE desktop environment.
     services.xserver.desktopManager.xfce.enable = true;
 
-    environment.systemPackages = with pkgs; [
-      xfce.xfce4-pulseaudio-plugin
+    environment.systemPackages = with pkgs; [ xfce.xfce4-pulseaudio-plugin ];
+    programs.thunar.plugins = with pkgs; [
       xfce.thunar-archive-plugin
       xfce.thunar-dropbox-plugin
     ];
+
+    services.blueman.enable = true;
 
     # Configure keymap in X11
     services.xserver.xkb = {
