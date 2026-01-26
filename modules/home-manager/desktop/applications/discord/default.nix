b74f0a661e977fe10d7d5e4492ecafe8c555be7a
svitax <svitaxiom@gmail.com>
2026-01-26 15:00:34 -0500

    discord: add openasar

--- modules/home-manager/desktop/applications/discord/default.nix
+++ modules/home-manager/desktop/applications/discord/default.nix
@@ -16,11 +16,7 @@ in
 
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
-      # (discord.override {
-      #   # withOpenASAR = true;
-      #   withVencord = true;
-      # })
-      discord
+      (discord.override { withOpenASAR = true; })
     ];
   };
 }
